d4e589755d6ffef1818fb25cecd5a88f9fad1a20
bobby chennamsetti <tejatech116@gmail.com>
2023-04-21 12:49:02 +0530

    adding printing statements to redis.sh

---
+++ redis.sh
@@ -0,0 +1,12 @@
+echo -e "\e[36m<<<<<<<<< install redis repos>>>>>>>>>>>>\e[om"
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+echo -e "\e[36m<<<<<<<<< install redis >>>>>>>>>>>>\e[om"
+dnf module enable redis:remi-6.2 -y
+yum install redis -y
+
+echo -e "\e[36m<<<<<<<<< update redis listen address >>>>>>>>>>>>\e[om"
+sed -i -e 's|127.0.0.0|0.0.0.0|' /etc/redis.conf
+
+echo -e "\e[36m<<<<<<<<< start redis service >>>>>>>>>>>>\e[om"
+systemctl enable redis
+systemctl start redis
