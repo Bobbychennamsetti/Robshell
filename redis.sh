e145da8da331f61df6cbf03bc0710bf7ed6f5e4d
bobby chennamsetti <tejatech116@gmail.com>
2023-04-21 12:58:18 +0530

    adding printing statements to redis.sh

--- redis.sh
+++ redis.sh
@@ -5,8 +5,8 @@ dnf module enable redis:remi-6.2 -y
 yum install redis -y
 
 echo -e "\e[36m<<<<<<<<< update redis listen address >>>>>>>>>>>>\e[om"
-sed -i -e 's|127.0.0.0|0.0.0.0|' /etc/redis.conf
+sed -i -e 's|127.0.0.0|0.0.0.0|' /etc/redis.conf /etc/redis/redis.conf
 
 echo -e "\e[36m<<<<<<<<< start redis service >>>>>>>>>>>>\e[om"
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
